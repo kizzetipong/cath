4d5b53cfab93c9911f512ac6750d3514589adef8
Kittipong Boonyapisomparn <kizzetipong@gmail.com>
2016-08-03 00:04:23 +0700

    example of angular service

--- app/scripts/components/catalogslider/catalogSliderController.js
+++ app/scripts/components/catalogslider/catalogSliderController.js
@@ -1,74 +1,12 @@
 'use strict';
 
 angular.module('angular-seed')
-.controller('catalogSliderController', ['$rootScope', '$scope', function ($rootScope, $scope) {
-  $scope.callData = function (cat) {
-    var deferred = new $.Deferred();
-    var mockData = {
-      news: [
-        {
-          classId: '',
-          headlineText: 'HEADLINE 1',
-          date: '',
-          img: '',
-          briefText: '',
-        },
-        {
-          classId: '',
-          headlineText: 'HEADLINE 2',
-          date: '',
-          img: '',
-          briefText: '',
-        },
-        {
-          classId: '',
-          headlineText: 'HEADLINE 3',
-          date: '',
-          img: '',
-          briefText: '',
-        },
-        {
-          classId: '',
-          headlineText: 'HEADLINE 4',
-          date: '',
-          img: '',
-          briefText: '',
-        },
-        {
-          classId: '',
-          headlineText: 'HEADLINE 5',
-          date: '',
-          img: '',
-          briefText: '',
-        },
-      ],
-      investigations: [
-        {
-          classId: '',
-          headlineText: 'investigations 1',
-          date: '',
-          img: '',
-          briefText: '',
-        },
-        {
-          classId: '',
-          headlineText: 'investigations 2',
-          date: '',
-          img: '',
-          briefText: '',
-        },
-      ],
-      settlements: [],
-    }
-    _.delay(function () {
-      $scope.dataReady = true;
-      deferred.resolve(_.get(mockData, cat));
-      $scope.$applyAsync();
-    }, 500);
-    return deferred.promise();
-  };
+.controller('catalogSliderController', ['$rootScope', '$scope', 'myService', function ($rootScope, $scope, myService) {
   $scope.dataReady = false;
-  $scope.callData($scope.catalog).then(function (ret) {
+  
+  myService.fetchData($scope.catalog).then(function (ret) {
+    $scope.dataReady = true;
     $scope.list = ret;
-  }); // try simulate timing like real service call
+    $scope.$applyAsync();
+  });
 }]);
--- app/scripts/services/myService.js
+++ app/scripts/services/myService.js
@@ -3,16 +3,75 @@
 angular.module('angular-seed')
 .service('myService', ['AppConfig',
   function ($appConfig) {
-    var metadata;
-    this.url = $appConfig.UDF_URL;
-
+    var data;
+    this.url = $appConfig.DB_URL;
     return {
-      requestMetadata: function () {
-        metadata = { data: 'MockData' };
-        return;
+      fetchData: function (cat) {
+        var deferred = new $.Deferred();
+        var mockData = {
+          news: [
+            {
+              classId: '',
+              headlineText: 'HEADLINE 1',
+              date: '',
+              img: '',
+              briefText: '',
+            },
+            {
+              classId: '',
+              headlineText: 'HEADLINE 2',
+              date: '',
+              img: '',
+              briefText: '',
+            },
+            {
+              classId: '',
+              headlineText: 'HEADLINE 3',
+              date: '',
+              img: '',
+              briefText: '',
+            },
+            {
+              classId: '',
+              headlineText: 'HEADLINE 4',
+              date: '',
+              img: '',
+              briefText: '',
+            },
+            {
+              classId: '',
+              headlineText: 'HEADLINE 5',
+              date: '',
+              img: '',
+              briefText: '',
+            },
+          ],
+          investigations: [
+            {
+              classId: '',
+              headlineText: 'investigations 1',
+              date: '',
+              img: '',
+              briefText: '',
+            },
+            {
+              classId: '',
+              headlineText: 'investigations 2',
+              date: '',
+              img: '',
+              briefText: '',
+            },
+          ],
+          settlements: [],
+        }
+        _.delay(function () { //TODO: should call real nodejs service
+          deferred.resolve(_.get(mockData, cat));
+          data = _.merge(data, _.get(mockData, cat));
+        }, 500);
+        return deferred.promise();
       },
-      getMetadata: function () {
-        return metadata;
+      getLatestData: function () {
+        return data;
       },
     };
   },
