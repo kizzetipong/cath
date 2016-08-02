e78d1cf553bcd9f386331cdc6a1fa35da04b23db
Kittipong Boonyapisomparn <kizzetipong@gmail.com>
2016-08-03 00:36:38 +0700

    example of call node service

--- service/mynodeservice.js
+++ service/mynodeservice.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var _ = require('lodash');
+
 function MyNodeService() {
 }
 
@@ -7,7 +9,68 @@ MyNodeService.prototype.init = function () {
 };
 
 MyNodeService.prototype.service = function (context, payload, serviceCallback) {
-  serviceCallback(null, 'This is return from node service');
+  if (!payload || !payload.catalog) {
+    serviceCallback('ERROR Msg', null);
+  }
+
+  var mockData = {
+    news: [
+      {
+        classId: '',
+        headlineText: 'HEADLINE 1',
+        date: '',
+        img: '',
+        briefText: '',
+      },
+      {
+        classId: '',
+        headlineText: 'HEADLINE 2',
+        date: '',
+        img: '',
+        briefText: '',
+      },
+      {
+        classId: '',
+        headlineText: 'HEADLINE 3',
+        date: '',
+        img: '',
+        briefText: '',
+      },
+      {
+        classId: '',
+        headlineText: 'HEADLINE 4',
+        date: '',
+        img: '',
+        briefText: '',
+      },
+      {
+        classId: '',
+        headlineText: 'HEADLINE 5',
+        date: '',
+        img: '',
+        briefText: '',
+      },
+    ],
+    investigations: [
+      {
+        classId: '',
+        headlineText: 'investigations 1',
+        date: '',
+        img: '',
+        briefText: '',
+      },
+      {
+        classId: '',
+        headlineText: 'investigations 2',
+        date: '',
+        img: '',
+        briefText: '',
+      },
+    ],
+    settlements: [],
+  };  //TODO: should retrieve data from real database
+  var retAry = mockData[payload.catalog] || [];
+  serviceCallback(null, retAry);
 };
 
 module.exports = MyNodeService;
