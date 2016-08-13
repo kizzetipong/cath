1862c4477f4db9d6e769e9ad108dd3b3826aba00
Kittipong Boonyapisomparn <kizzetipong@gmail.com>
2016-08-14 00:05:30 +0700

    use data from mocked service

--- app/scripts/components/introslider/introSliderController.js
+++ app/scripts/components/introslider/introSliderController.js
@@ -7,13 +7,36 @@ angular.module('cath')
   $scope.calcWidth = $(window).width();
   
   myService.fetchData($scope.catalog).then(function (ret) {
+    // TODO: mockData
+    ret = [
+      {
+        image: '/assets/images/marketing/1-1.jpg',
+        header: 'EQUAL . JUSTIC . UNDER . LAW',
+        content: 'We have served clients of all range, from individuals to big organization. We provide personal solutions based on one\'s situation. Contact us for more information.',
+      },
+      {
+        image: '/assets/images/marketing/1-2.jpg',
+        header: 'WE OFFER OUR CLIENTS ONLY',
+        content: 'We have served clients of all range, from individuals to big organization. We provide personal solutions based on one\'s situation. Contact us for more information.',
+      },
+      {
+        image: '/assets/images/marketing/1-1.jpg',
+        header: 'EQUAL . JUSTIC . UNDER . LAW',
+        content: 'We have served clients of all range, from individuals to big organization. We provide personal solutions based on one\'s situation. Contact us for more information.',
+      },
+      {
+        image: '/assets/images/marketing/1-2.jpg',
+        header: 'WE OFFER OUR CLIENTS ONLY',
+        content: 'We have served clients of all range, from individuals to big organization. We provide personal solutions based on one\'s situation. Contact us for more information.',
+      },
+    ];
     $scope.dataReady = true;
     $scope.list = ret;
     $scope.$applyAsync();
   });
   $(window).resize(function () {
-  	$scope.calcHeight = $(window).width() / 2;
-  	$scope.calcWidth = $(window).width();
-  	$scope.$applyAsync();
+    $scope.calcHeight = $(window).width() / 2;
+    $scope.calcWidth = $(window).width();
+    $scope.$applyAsync();
   });
 }]);
