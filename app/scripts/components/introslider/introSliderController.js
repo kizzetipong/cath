33da967406cd9d9fb06d23f98093c285bfbb04d5
Kittipong Boonyapisomparn <kizzetipong@gmail.com>
2016-08-13 23:53:53 +0700

    improve introSlider to be responsive

--- app/scripts/components/introslider/introSliderController.js
+++ app/scripts/components/introslider/introSliderController.js
@@ -3,10 +3,17 @@
 angular.module('cath')
 .controller('introSliderController', ['$rootScope', '$scope', 'myService', function ($rootScope, $scope, myService) {
   $scope.dataReady = false;
+  $scope.calcHeight = 620;
+  $scope.calcWidth = $(window).width();
   
   myService.fetchData($scope.catalog).then(function (ret) {
     $scope.dataReady = true;
     $scope.list = ret;
     $scope.$applyAsync();
+  });
+  $(window).resize(function () {
+  	$scope.calcHeight = $(window).width() / 2;
+  	$scope.calcWidth = $(window).width();
+  	$scope.$applyAsync();
   });
 }]);
