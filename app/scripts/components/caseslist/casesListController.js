dc586efa4f0f0afb148faf707902a26e85054967
Kittipong Boonyapisomparn <kizzetipong@gmail.com>
2016-09-12 18:41:51 +0700

    - edit layout

--- app/scripts/components/caseslist/casesListController.js
+++ app/scripts/components/caseslist/casesListController.js
@@ -3,10 +3,10 @@
 angular.module('cath')
 .controller('casesListController', ['$rootScope', '$scope', 'myService', function ($rootScope, $scope, myService) {
   $scope.dataReady = false;
-  
+
   myService.fetchData('cases').then(function (ret) {
     $scope.dataReady = true;
-    $scope.list = ($scope.stage === 'all') ? ret : _.filter(ret, {stage: $scope.stage});
+    $scope.list = ($scope.stage === 'all') ? ret : _.filter(ret, { stage: $scope.stage });
     $scope.$applyAsync();
   });
 }]);
