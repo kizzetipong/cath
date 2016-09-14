d4d893389c88cfb57aec684a180544bd76717deb
Kittipong Boonyapisomparn <kizzetipong@gmail.com>
2016-09-15 00:16:21 +0700

    mock pic

--- app/scripts/components/introslider/introSliderController.js
+++ app/scripts/components/introslider/introSliderController.js
@@ -3,14 +3,16 @@
 angular.module('cath')
 .controller('introSliderController', ['$rootScope', '$scope', 'myService', function ($rootScope, $scope, myService) {
   $scope.dataReady = false;
+  // $scope.imageScale = 1/2;
+  var imageScale = 1/3;
   $scope.calcWidth = $(window).width();
-  $scope.calcHeight = $scope.calcWidth / 2;
+  $scope.calcHeight = $scope.calcWidth * imageScale;
 
   myService.fetchData('news').then(function (ret) {
     // TODO: mockData
     ret = [
       {
-        image: '/assets/images/marketing/1-1.jpg',
+        image: '/assets/images/marketing/title.jpg',
         header: 'EQUAL . JUSTIC . UNDER . LAW',
         content: 'We have served clients of all range, from individuals to big organization. We provide personal solutions based on one\'s situation. Contact us for more information.',
       },
@@ -45,7 +47,7 @@ angular.module('cath')
   });
   $(window).resize(function () {
     $scope.calcWidth = $(window).width();
-    $scope.calcHeight = $scope.calcWidth / 2;
+    $scope.calcHeight = $scope.calcWidth * imageScale;
     $scope.$applyAsync();
   });
 }]);
