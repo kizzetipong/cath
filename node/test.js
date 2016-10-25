8781c46317acb156ca938e87643f4a1aefc30c31
Kittipong Boonyapisomparn <kizzetipong@gmail.com>
2016-10-25 23:43:36 +0700

    improve articleDisplay component - social container

--- node/test.js
+++ node/test.js
@@ -3,7 +3,7 @@
     var connection = mysql.createConnection({
       host     : '127.0.0.1',
       port     : 3306,
-      user     : 'root',
+      user     : 'classaction',
       password : 'pwd123',
       database : 'class_action'
     });
