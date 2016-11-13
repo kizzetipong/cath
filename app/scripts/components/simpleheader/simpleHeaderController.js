05229dd210afe5598b5dcc613c8ed56a69a600df
Kittipong Boonyapisomparn <kizzetipong@gmail.com>
2016-11-13 23:28:30 +0700

    - support menu=false - adjust social image's size

--- app/scripts/components/simpleheader/simpleHeaderController.js
+++ app/scripts/components/simpleheader/simpleHeaderController.js
@@ -1,8 +1,10 @@
 'use strict';
 
 angular.module('cath')
-.controller('simpleHeaderController', ['$rootScope', '$scope', function ($rootScope, $scope) {
+.controller('simpleHeaderController', ['$rootScope', '$scope', '$location', function ($rootScope, $scope, $location) {
+  $scope.params = $location.search();
   $scope.theme = $scope.bgRed === 'red' ? 'white' : 'red';
   $scope.expandMenu = false;
   $scope.expandSearch = false;
+  $scope.hideMenu = $scope.params.menu === 'false' || false;
 }]);
